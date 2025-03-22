75a64937e06bcdfeaa4de1853fcef69ef9487fe0
Robart Khaling Rai <121083387+rairobart@users.noreply.github.com>
2025-03-23 00:08:33 +0545

    Add files via upload

---
+++ Robart/script.js
@@ -0,0 +1,4 @@
+// Redirect to another page when the button is clicked
+document.getElementById('beginButton').addEventListener('click', function() {
+    window.location.href = 'another-page.html'; // Replace with your desired URL
+  });
